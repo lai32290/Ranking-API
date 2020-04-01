9670b5410b5003501e06e6faa51ea3cf046fad6f
antonio hl santos <toninho0007@yahoo.com.br>
2020-03-31 23:28:34 -0300

    Start with database

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,31 @@
 #![feature(decl_macro)]
 #![feature(proc_macro_hygiene)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use postgres::{Client, NoTls};
+use rocket::State;
+use std::sync::Mutex;
+
+struct DB {
+    client : Mutex<Client>
+}
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn hello(db : State<DB>) -> &'static str {
+    db.client.lock().unwrap().execute("INSERT INTO app (name) VALUES ('Test')",&[]);
+    return "Hello, world!"
 }
+
 #[get("/2")]
 fn hello2() -> &'static str {
     "Hello, world!2"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello,hello2]).launch();
+
+    let mut client = Client::connect("host=localhost user=postgres password=postgres", NoTls).unwrap();
+    client.execute("INSERT INTO app (name) VALUES ('Test')",&[]);
+    rocket::ignite().manage(DB{client:Mutex::new(client)}).mount("/", routes![hello,hello2]).launch();
 }
