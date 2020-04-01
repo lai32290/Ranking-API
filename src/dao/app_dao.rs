cbcad942e1d0682a4c3056d594654b8719b6a33e
antonio hl santos <toninho0007@yahoo.com.br>
2020-04-01 00:30:14 -0300

    First DAO

---
+++ src/dao/app_dao.rs
@@ -0,0 +1,46 @@
+use postgres::{NoTls, Client, Error, Row};
+use r2d2_postgres::{PostgresConnectionManager, r2d2};
+use r2d2::Pool;
+
+pub struct AppDao{
+    pool: Pool<PostgresConnectionManager<NoTls>>
+}
+
+impl AppDao {
+    pub fn new(pool :Pool<PostgresConnectionManager<NoTls>>) -> AppDao{
+        AppDao{pool}
+    }
+
+    pub fn exists(&self, name: String) -> bool{
+        return match self.pool.clone().get().unwrap().query("select count() from app where name = $1;", &[&name]) {
+            Ok(rows) => {
+                match rows.first() {
+                    Some(row) => {
+                        let count: i32 = row.get(0);
+                        if count > 0 {
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                    _ => { false }
+                }
+            }
+            _ => { false }
+        }
+    }
+
+    pub fn create(&self, name: String) -> Option<String>{
+        return match self.pool.clone().get().unwrap().query("insert into app (name) values ($1) returning id::text;",&[&name]) {
+            Ok(rows) => {
+                match rows.first() {
+                    None => {None},
+                    Some(row) => {
+                        return Some(row.get(0));
+                    },
+                }
+            },
+            Err(_) => {None},
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,46 @@
 /<app_token>/score/<user_id>/add?score=100
 /<app_token>/score/<user_id>
 */
-#![feature(decl_macro)]
-#![feature(proc_macro_hygiene)]
+#![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
-use postgres::{Client, NoTls};
+use crate::dao::app_dao;
+
+mod dao;
+
+use postgres::{NoTls, Client};
+use r2d2_postgres::{PostgresConnectionManager, r2d2};
 use rocket::State;
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
+use r2d2::Pool;
+use serde::Serialize;
+use rocket_contrib::json::{Json, JsonValue};
+use crate::dao::app_dao::AppDao;
 
-struct DB {
-    client : Mutex<Client>
+struct DAO {
+    app: Arc<AppDao>
 }
 
-#[get("/")]
-fn hello(db : State<DB>) -> &'static str {
-    db.client.lock().unwrap().execute("INSERT INTO app (name) VALUES ('Test')",&[]);
-    return "Hello, world!"
+
+#[derive(Serialize, Deserialize)]
+struct AppRequest {
+    name: String
+}
+
+#[post("/register_app", format = "json", data = "<request>")]
+fn hello(dao: State<DAO>, request: Json<AppRequest>) -> JsonValue {
+    if dao.app.exists(request.0.name.clone()) {
+        return json!({ "status": "NOK" });
+    }
+    return match dao.app.create(request.0.name.clone()) {
+        None => { json!({ "status": "NOK" }) }
+        Some(id) => {
+            json!({ "status":id.as_str()})
+        }
+    };
 }
 
 #[get("/2")]
@@ -30,8 +52,14 @@ fn hello2() -> &'static str {
 }
 
 fn main() {
+    let manager = PostgresConnectionManager::new(
+        "host=localhost user=postgres password=postgres".parse().unwrap(),
+        NoTls,
+    );
+    let pool = r2d2::Pool::new(manager).unwrap();
 
-    let mut client = Client::connect("host=localhost user=postgres password=postgres", NoTls).unwrap();
-    client.execute("INSERT INTO app (name) VALUES ('Test')",&[]);
-    rocket::ignite().manage(DB{client:Mutex::new(client)}).mount("/", routes![hello,hello2]).launch();
+    rocket::ignite()
+        .manage(DAO { app: Arc::new(AppDao::new(pool)) })
+        .mount("/", routes![hello,hello2])
+        .launch();
 }
